e53a017af34c4f958b16c4b8259d0bbd925fa76e
MauroBorzi <mauroborzi91@gmail.com>
2025-07-10 12:19:35 -0400

    [upd]style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 const button = document.getElementById(`send`)
 const reset = document.getElementById(`reset`)
 
-// creo l'evento click sul pulsante
+// creo gli eventi click sui pulsanti
 
 reset.addEventListener(`click`, function (e) {
   e.preventDefault()
@@ -22,15 +22,19 @@ button.addEventListener(`click`, function (e) {
   const age = document.getElementById(`age`).value
 
   // definizione variabili
+
   let priceTicket = kilo * 0.21
 
   // sconto under e over
+
   if (age === "Biglietto Under") {
     priceTicket = priceTicket * (1 - 0.20)
   }
   else if (age === "Biglietto Over") {
     priceTicket = priceTicket * (1 - 0.40)
   }
+
+  // innietto i valori nel HTML
 
   document.getElementById(`show-name`).innerText = name
   document.getElementById(`show-kilo`).innerText = kilo + `Km`
